74051c372f0a1ad185c608070a292578d4268f05
Gary Qian <biosmasher@meloncraft.com>
2014-08-10 19:20:55 -0400

    Initial setup
    Creation of main class and plugin.yml along with pom.xml initial setup

---
+++ src/main/java/com/meloncraft/league/League.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.meloncraft.league;
+
+/**
+ *
+ * @author Gary
+ */
+public class League {
+    
+}
