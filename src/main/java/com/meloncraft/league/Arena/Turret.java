413b10b559d5cb48218b59cd0b9a69bfc6b550c7
Gary Qian <jeegary@gmail.com>
2014-08-13 00:12:37 -0400

    Create turret Body

--- src/main/java/com/meloncraft/league/Arena/Turret.java
+++ src/main/java/com/meloncraft/league/Arena/Turret.java
@@ -17,12 +17,57 @@ import org.bukkit.World;
  */
 public class Turret {
     League plugin;
-    public Location center;
+    public Location center, temp;
     public World world;
     public boolean team; //TRUE = blue FALSE = purple
     public List<Location> turretBody;
     
     public Turret(Location cent, boolean tea) {
         
+        addColumnToBody(cent);
+        
+        temp = cent;
+        temp.setX(temp.getBlockX() - 1);
+        addColumnToBody(temp);
+        
+        temp = cent;
+        temp.setX(temp.getBlockX() + 1);
+        addColumnToBody(temp);
+        
+        temp.setZ(temp.getBlockZ() - 1);
+        addColumnToBody(temp);
+        
+        temp = cent;
+        temp.setZ(temp.getBlockZ() + 1);
+        addColumnToBody(temp);
+        
+        temp = cent;
+        temp.setX(temp.getBlockX() - 1);
+        temp.setZ(temp.getBlockZ() - 1);
+        addColumnToBody(temp);
+        
+        temp = cent;
+        temp.setX(temp.getBlockX() + 1);
+        temp.setZ(temp.getBlockZ() + 1);
+        addColumnToBody(temp);
+        
+        temp = cent;
+        temp.setX(temp.getBlockX() + 1);
+        temp.setZ(temp.getBlockZ() - 1);
+        addColumnToBody(temp);
+        
+        temp = cent;
+        temp.setX(temp.getBlockX() - 1);
+        temp.setZ(temp.getBlockZ() + 1);
+        addColumnToBody(temp);
+    }
+    
+    public void addColumnToBody(Location loc) {
+        //The number counted to is the height
+        for (int count = 0; count < 5; count++) {
+            temp = loc;
+            temp.setY(loc.getBlockY() - count);
+            turretBody.add(temp);
+        }
     }
 }
